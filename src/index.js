0598f762dba974687cf49f4058a3391d2a330530
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2021-02-09 15:51:15 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyCsQVFY3Ou5SFOAtzSV5E8AjjhbfNaJ-oM",
+    authDomain: "evernote-clone-1d810.firebaseapp.com",
+    projectId: "evernote-clone-1d810",
+    storageBucket: "evernote-clone-1d810.appspot.com",
+    messagingSenderId: "1073472148057",
+    appId: "1:1073472148057:web:1965058d76985408185748",
+    measurementId: "G-T8PPNGFWXJ"
+  };
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
